907694125892a6534a10135696c8e899cd5bec46
sidikou <sidikoumoussaanaroua2000@gmail.com>
2026-04-02 17:27:39 +0100

    Fix CORS pour Render

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({  origin:  ['http://localhost:5500', 'http://0.0.0.0:5500'] }));
-app.use(express.json({limit:'50mb'}));
+// ✅ Autoriser toutes les origines (pour Render)
+app.use(cors({ origin: '*' }));
+
+app.use(express.json({ limit: '50mb' }));
 app.use('/api', routes);
 
-app.listen(port, ()=>{
-	console.log(`Serveur démarré sur http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
 });
